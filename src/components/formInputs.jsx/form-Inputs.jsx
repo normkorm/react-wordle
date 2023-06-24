1c256a1f62ea4436ec95f2f2e6c35b90256cb669
web-aldiyar <web.aldiyar@protonmail.com>
2023-06-24 23:16:39 +0600

    change events

--- src/components/formInputs.jsx/form-Inputs.jsx
+++ src/components/formInputs.jsx/form-Inputs.jsx
@@ -1,32 +1,35 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 export const FormInputs = () => {
+  const [word, setWord] = useState("");
   const inputRefs = Array.from({ length: 25 }, () => useRef(null));
   const handleFocus = (e, index) => {
     const regex = /^[А-яЁё]+$/;
     if (!regex.test(e.target.value)) {
       inputRefs[index].current.value = "";
     } else {
-      inputRefs[index + 1]?.current.removeAttribute("readOnly", "readOnly");
-      if (index !== inputRefs.length - 1)
-        inputRefs[index].current.setAttribute("readOnly", "readOnly");
+      if ((index + 1) % 5 !== 0) {
+        setWord((prevState) => prevState + e.target.value);
+        inputRefs[index + 1]?.current.removeAttribute("disabled", "disabled");
+        inputRefs[index + 1]?.current.focus();
+        inputRefs[index].current.setAttribute("disabled", "disabled");
+      }
       inputRefs[index].current.value =
         inputRefs[index].current.value.toUpperCase();
-      inputRefs[index + 1]?.current.focus();
     }
   };
-
   const handleKeyFocus = (e, index) => {
     if (e.code === "Backspace" && inputRefs[index].current.value.length === 0) {
       if (index !== 0)
-        inputRefs[index].current.setAttribute("readOnly", "readOnly");
-      inputRefs[index - 1]?.current.removeAttribute("readOnly", "readOnly");
+        inputRefs[index].current.setAttribute("disabled", "disabled");
+      inputRefs[index - 1]?.current.removeAttribute("disabled", "disabled");
       inputRefs[index - 1]?.current.focus();
     }
-  };
-
-  const checkWord = (word) => {
-    if (word.length === 5) console.log("good");
+    if (e.code === "Enter" && (index + 1) % 5 === 0) {
+      inputRefs[index]?.current.setAttribute("disabled", "disabled");
+      inputRefs[index + 1]?.current.removeAttribute("disabled", "disabled");
+      inputRefs[index + 1]?.current.focus();
+    }
   };
   return (
     <form className="grid grid-cols-5 gap-2">
@@ -34,7 +37,7 @@ export const FormInputs = () => {
         <input
           key={index}
           className="border-2 text-center w-14 h-14 focus:outline-none font-black"
-          readOnly={index !== 0}
+          disabled={index !== 0}
           autoFocus={index === 0}
           maxLength={1}
           ref={ref}
